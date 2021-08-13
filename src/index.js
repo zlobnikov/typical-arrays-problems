43cccd34f201bf16d547f2356542a67773278a6a
zlobnikov <daffodil@list.ru>
2021-08-13 10:36:26 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  let minValue = array[0];
+  for (let i = 1; i < array.length; ++i) {
+    if (minValue > array[i]) minValue = array[i];
+  }
+
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  let maxValue = array[0];
+  for (let i = 1; i < array.length; ++i) {
+    if (maxValue < array[i]) maxValue = array[i];
+  }
+
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  let accumulator = 0;
+  for (let i = 0; i < array.length; ++i) {
+    accumulator += array[i];
+  }
+
+  return accumulator / array.length;
 }
